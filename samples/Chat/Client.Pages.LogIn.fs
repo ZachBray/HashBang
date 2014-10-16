513739f7d0f7bfbde0cef1133273cd77ebaa6f2f
Zach Bray <zachbray@googlemail.com>
2014-10-17 00:55:38 +0100

    WIP Added bits to the log in form.

--- samples/Chat/Client.Pages.LogIn.fs
+++ samples/Chat/Client.Pages.LogIn.fs
@@ -10,12 +10,21 @@ type LogInPage() =
     
     let currentUri = Routes.Session.LogIn.CreateUri() 
 
+    let textInput placeholder inputType addons = 
+        !![Bootstrap.input_group; Bootstrap.col_lg_6; Bootstrap.col_lg_offset_3] ==> [
+            Span.empty +. [Bootstrap.input_group_addon] 
+            |> Element.style "width: 20%;" |> Element.append addons
+            Input.empty +. [Bootstrap.form_control] |> Input.``type`` inputType
+            |> Input.placeholder placeholder
+        ]
+
     let createPage() =
         Templates.insideNavBar
             currentUri
             "Log In" 
             "Please fill in your email address and password or sign-up for a new account." [
-            P.empty --> "TODO: ADD CONTENT"
+            P.empty ==> [textInput "joebloggs@gmail.com" IInputElementType.TextCase [Text "Email"]]
+            P.empty ==> [textInput "P4ssW0rD" IInputElementType.Password [Text "Pass"]]
         ]
 
     interface IPage with
