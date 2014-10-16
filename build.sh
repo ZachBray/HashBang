788de989319f07802bf868f4e3e5f923bcbd911b
Zach Bray <zachbray@googlemail.com>
2014-10-16 13:12:02 +0100

    Added some printing to the build.

--- build.sh
+++ build.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+echo Starting...
 if test "$OS" = "Windows_NT"
 then
   # use .Net
-
+  echo .NET build...
   .paket/paket.bootstrapper.exe
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
+    echo Command failed:  paket.bootstrapper.exe
   	exit $exit_code
   fi
 
   .paket/paket.exe restore
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
+    echo Command failed: paket.exe restore
   	exit $exit_code
   fi
   
@@ -20,15 +23,18 @@ then
   packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
 else
   # use mono
+  echo mono build...
   mono .paket/paket.bootstrapper.exe
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
+    echo Command failed: paket.bootstrapper.exe
   	exit $exit_code
   fi
 
   mono .paket/paket.exe restore
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
+    echo Command failed: paket.exe restore
   	exit $exit_code
   fi
 
