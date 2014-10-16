b98d58599d2d3e5c82c473d91072f7c5988d9022
Zach Bray <zachbray@googlemail.com>
2014-10-16 12:47:56 +0100

    Bump version to 0.3.1-alpha

--- src/TypeInferred.HashBang.SignalR/AssemblyInfo.fs
+++ src/TypeInferred.HashBang.SignalR/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("TypeInferred.HashBang.SignalR")>]
 [<assembly: AssemblyProductAttribute("HashBang")>]
 [<assembly: AssemblyDescriptionAttribute("A collection of libraries for building web and mobile applications. Leverages FunScript for seamless full-stack development in F#. Adheres to Google's standard for ajax=crawling.")>]
-[<assembly: AssemblyVersionAttribute("0.2.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
+[<assembly: AssemblyVersionAttribute("0.3.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.3.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.2.0"
+    let [<Literal>] Version = "0.3.1"
